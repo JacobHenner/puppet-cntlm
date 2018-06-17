ca1df0cc57a8db243d2549ee2d5114c8fdc810ab
Jacob Henner <code@ventricle.us>
2018-06-17 00:20:51 -0400

    Migrate default configuration parameters to init.pp, remove module hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,23 +12,23 @@
 #       cntlm_proxy    => ['localhost:8080'],
 #   }
 #
-# @param package_manage Whether to manage the cntlm package. Default value: true
-# @param package_name Specifies the cntlm package to manage. Default value: 'cntlm'
-# @param package_ensure Whether to install the cntlm package, or a specific version to install. Values: 'present', 'latest', or a specific version.
-# @param service_manage Whether to manage the cntlm service. Default value: true.
-# @param service_ensure Whether the cntlm service should be running. Default value: 'running'
-# @param service_enable Whether to enable the cntlm service at boot. Default value" true.
-# @param service_name The cntlm service to manage. Default value: cntlm
-# @param service_provider Which service provider to use for cntlm. Default value: Undef.
 # @param cntlm_username The username to use for proxy authentication.
 # @param cntlm_domain The domain to use for proxy authentication.
+# @param cntlm_proxy A list of parent proxies to use in format ['proxy_ip:proxy_port', ...]
+# @param package_manage Whether to manage the cntlm package.
+# @param package_name Specifies the cntlm package to manage.
+# @param package_ensure Whether to install the cntlm package, or a specific version to install. Values: 'present', 'latest', or a specific version.
+# @param service_manage Whether to manage the cntlm service.
+# @param service_ensure Whether the cntlm service should be running.
+# @param service_enable Whether to enable the cntlm service at boot.
+# @param service_name The cntlm service to manage. Default value: cntlm
+# @param service_provider Which service provider to use for cntlm.
 # @param cntlm_password The cleartext password used for proxy authentication. Other password methods are preferred.
 # @param cntlm_passlm The PassLM password hash used for proxy authentication.
 # @param cntlm_passnt The PassNT password hash used for proxy authentication.
 # @param cntlm_passntlmv2 The PassNTLMv2 password hash used for proxy authentication.
 # @param cntlm_workstation The netbios hostname cntlm will send to the parent proxies. If unset, the value is auto-guessed by cntlm.
-# @param cntlm_proxy A list of parent proxies to use in format ['proxy_ip:proxy_port', ...]
-# @param cntlm_noproxy A comma separated list of addresses that will not be passed through parent proxyies. * and ? wildcards can be used.
+# @param cntlm_noproxy An array of addresses that will not be passed through parent proxies. * and ? wildcards can be used.
 # @param cntlm_listen The port(s) cntlm should listen on. Can also specify multiple specific interface(s) and port(s) to bind to, using 'ip:port'.
 # @param cntlm_socks5proxy A list of interfaces and ports to bind to for SOCKS5 proxy functionality,  in format ['ip:port', ...]
 # @param cntlm_socks5user A list of username:password pairs for permissioning access to the SOCKS5 proxy. If unset, the SOCKS5 proxy will accept all requests.
@@ -42,35 +42,35 @@
 # @param cntlm_header A list of headers to replace if present in the request.
 # @param cntlm_tunnel A list of tunnels mapping local ports to machines behind the proxy.
 class cntlm (
-    Boolean $package_manage,
-    String[1] $package_name,
-    String[1] $package_ensure,
-    Boolean $service_manage,
-    Enum['running', 'stopped'] $service_ensure,
-    Boolean $service_enable,
-    String[1] $service_name,
-    Optional[String[1]] $service_provider,
     String[1] $cntlm_username,
     String[1] $cntlm_domain,
-    Optional[String[1]] $cntlm_password,
-    Optional[String[1]] $cntlm_passlm,
-    Optional[String[1]] $cntlm_passnt,
-    Optional[String[1]] $cntlm_passntlmv2,
-    Optional[String[1]] $cntlm_workstation,
     Array[String[1]] $cntlm_proxy,
-    Optional[String[1]] $cntlm_noproxy,
-    Optional[Array[String[1]]] $cntlm_listen,
-    Optional[Array[String[1]]] $cntlm_socks5proxy,
-    Optional[Array[String[1]]] $cntlm_socks5user,
-    Optional[String[1]] $cntlm_auth,
-    Optional[String[1]] $cntlm_flags,
-    Boolean $cntlm_gateway,
-    Optional[Array[String[1]]] $cntlm_allow,
-    Optional[Array[String[1]]] $cntlm_deny,
-    Optional[Integer] $cntlm_isascannersize,
-    Optional[Array[String[1]]] $cntlm_isascanneragent,
-    Optional[Array[String[1]]] $cntlm_header,
-    Optional[Array[String[1]]] $cntlm_tunnel,
+    Boolean $package_manage = true,
+    String[1] $package_name = 'cntlm',
+    String[1] $package_ensure = 'present',
+    Boolean $service_manage = true,
+    Enum['running', 'stopped'] $service_ensure = 'running',
+    Boolean $service_enable = true,
+    String[1] $service_name = 'cntlm',
+    Optional[String[1]] $service_provider = undef,
+    Optional[String[1]] $cntlm_password = undef,
+    Optional[String[1]] $cntlm_passlm = undef,
+    Optional[String[1]] $cntlm_passnt = undef,
+    Optional[String[1]] $cntlm_passntlmv2 = undef,
+    Optional[String[1]] $cntlm_workstation = undef,
+    Optional[Array[String[1]]] $cntlm_noproxy = ['localhost', '127.0.0.*', '10.*', '192.168.*'],
+    Optional[Array[String[1]]] $cntlm_listen = ['3128'],
+    Optional[Array[String[1]]] $cntlm_socks5proxy = undef,
+    Optional[Array[String[1]]] $cntlm_socks5user = undef,
+    Optional[String[1]] $cntlm_auth = undef,
+    Optional[String[1]] $cntlm_flags = undef,
+    Boolean $cntlm_gateway = false,
+    Optional[Array[String[1]]] $cntlm_allow = undef,
+    Optional[Array[String[1]]] $cntlm_deny = undef,
+    Optional[Integer] $cntlm_isascannersize = undef,
+    Optional[Array[String[1]]] $cntlm_isascanneragent = undef,
+    Optional[Array[String[1]]] $cntlm_header = undef,
+    Optional[Array[String[1]]] $cntlm_tunnel = undef,
 ) {
     contain cntlm::install
     contain cntlm::config
